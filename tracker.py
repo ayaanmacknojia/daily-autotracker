3e9dd4c29816a79904dee5244b2676b2c6de5bc9
Ayaan Macknojia <298392420+ayaanmacknojia@users.noreply.github.com>
2026-06-30 19:33:19 -0400

    add morning prompt input

---
+++ tracker.py
@@ -0,0 +1,13 @@
+print("--- MORNING SYSTEM CHECK ---")
+
+# gathering morning inputs
+sleepHours = input("How many hours did you sleep last night? ")
+gymToday = input("Gym today? (y/n): ")
+wakingMood = input("How do you feel after waking up? ")
+goalToday = input("What is one goal for today? ")
+
+print("\n\n[MORNING STATS SUMMARY]")
+print("Hours slept: " + sleepHours + " hr")
+print("Gym session today: " + gymToday)
+print("Mood after waking: " + wakingMood)
+print("Today's goal: " + goalToday)
