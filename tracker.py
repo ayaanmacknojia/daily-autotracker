854b1927e5688d8841b122c0554b42fba1099e15
Ayaan Macknojia <298392420+ayaanmacknojia@users.noreply.github.com>
2026-07-01 19:59:33 -0400

    convert storage to JSON

--- tracker.py
+++ tracker.py
@@ -1,3 +1,4 @@
+import json
 
 morning_or_evening = input("Are you running this in the morning or evening? Warning: if two identical times of the day are inputted consecutively, data will be null for other time of day (m/e): ")
 message_for_morning = None
@@ -13,6 +14,20 @@ hours_worked = None
 mood_evening = None
 goal_achieved = None
 message_for_morning = None
+
+stats_dict = {}
+
+try:
+    with open("daily_data.json", "r") as file:
+        stats_dict = json.load(file)
+        sleep_hours = stats_dict["sleep_hours"]
+        gym_today = stats_dict["gym_today"]
+        waking_mood = stats_dict["waking_mood"]
+        goal_today = stats_dict["goal_today"]
+
+except FileNotFoundError:
+    pass
+
 
 if morning_or_evening == "m":
     print("--- MORNING SYSTEM CHECK ---")
@@ -54,7 +69,25 @@ else:
     print(f"Mood tonight: {mood_evening}")
     print(f"Today's goal achieved: {goal_achieved}")
 
-    
+
+# add all stats to dictionary
+stats_dict["sleep_hours"] = sleep_hours
+stats_dict["gym_today"] = gym_today
+stats_dict["waking_mood"] = waking_mood
+stats_dict["goal_today"] = goal_today
+
+stats_dict["win_today"] = win_today
+stats_dict["hours_worked"] = hours_worked
+stats_dict["mood_evening"] = mood_evening
+stats_dict["goal_achieved"] = goal_achieved
+stats_dict["morning_message"] = message_for_morning
+
+
+# dump dictionary object to json text file
+with open("daily_data.json", "w") as file:
+    json.dump(stats_dict, file)
+
+'''
 with open("day_save.txt", "a") as file:
 
     file.write("\n\n[FULL DAY SUMMARY]\n")
@@ -70,7 +103,6 @@ with open("day_save.txt", "a") as file:
     file.write(f"Hours worked today: {hours_worked} hr\n")
     file.write(f"Mood tonight: {mood_evening}\n")
     file.write(f"Today's goal achieved: {goal_achieved}\n")
-
-
+'''
 
 
